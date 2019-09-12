b651f0f685228d44f812a5c3e0d2123b933be5fa
Eric Barefoot <eabarefoot@gmail.com>
2019-09-12 15:58:50 -0400

    add src scipts for figures

---
+++ figures/src/paleo_geom_plot.r
@@ -0,0 +1,19 @@
+# function(s) to make a plot of data from 3D model mapping data and field data.
+# specifically the data on kinematics vs geometric estimations
+# Eric Barefoot
+# Sep 2019
+
+# first, load libraries
+
+library(tidyverse)
+library(gtable)
+
+# load in the data
+
+read_csv() # outcrop data
+
+# make plot with field data of geometry-based paleohydraulic estimates
+
+ggplot() +
+
+# add panels with kinematic estimates.
---
+++ figures/src/preservation_plot.r
@@ -0,0 +1,18 @@
+# function(s) to make a plot of data from 3D model mapping data.
+# specifically the data on preservation relating it to bypass
+# Eric Barefoot
+# Sep 2019
+
+# first, load libraries
+
+library(tidyverse)
+library(gtable)
+
+# load in the data
+
+read_csv() # outcrop data
+read_csv() # model outputs
+
+# make plot with lines for model data
+
+ggplot() + 
