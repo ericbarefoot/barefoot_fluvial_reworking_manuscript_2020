4ebd8c8277d9ea1bb575ae27cd8dcd42e0d0e69d
Eric Barefoot <eabarefoot@gmail.com>
2019-09-26 15:25:30 -0500

    feat: add way to include data about whole model and join with field data.
    two related tasks. first was to make it so that when measurements made 
    on the model-derived shapefiles were joined with field data, they had 
    the right fields to be joined on. 
    
    the second was to find an easy way to include information that was the 
    same across all shapefile shapes. This was accomplished with a separate 
    file in the model data directory, which gets read in and copied across 
    ll rows.

--- analysis/piceance_basin_io.r
+++ analysis/piceance_basin_io.r
@@ -147,19 +147,24 @@ dirs_to_iterate = list.dirs(model_data_file, recursive = FALSE)
 files = vector('list', length = length(dirs_to_iterate))
 joined_tab = vector('list', length = length(dirs_to_iterate))
 attribute_tab = vector('list', length = length(dirs_to_iterate))
+site_attributes = vector('list', length = length(dirs_to_iterate))
 j = 1
 
 for (i in dirs_to_iterate) {
 	filelist = list.files(i)
 	if (length(filelist > 0))
 	{
-		match = grep('*.shp',filelist)
+		site_match = grep('*site_vals.csv', filelist)
+		sa = read_csv(file.path(i, filelist[site_match]), col_types = "Dccc")
+		match = grep('*.shp', filelist)
 		files[[j]] = st_read(file.path(i, filelist[match]))
 		files[[j]] = st_transform(files[[j]], 2152) # transform into UTM coordinates for 12 N
 		geom_tab = st_coordinates(files[[j]]) %>% as.data.frame()
 		att_tab = as.data.frame(files[[j]]) %>% select(-geometry)
 		att_tab$FID = att_tab$FID + 1
-		attribute_tab[[j]] = att_tab %>% rename(id = 'FID', interpretations = 'LAYER') %>% mutate(id = id + (j * 1000))
+		attribute_tab[[j]] = att_tab %>% rename(id = 'FID') %>% select(-LAYER) %>% 
+		mutate(id = id + (j * 1000)) %>% 
+		mutate(wp_num = sa$wp_num, date = sa$date, formation = sa$formation, field_site = sa$field_site)
 		joined_tab[[j]] = inner_join(att_tab, geom_tab, by = c('FID' = 'L1'))
 		joined_tab[[j]] = joined_tab[[j]] %>% rename(id = 'FID', interpretations = 'LAYER')
 		joined_tab[[j]]$id = joined_tab[[j]]$id + (j * 1000)
@@ -172,7 +177,7 @@ for (i in dirs_to_iterate) {
 }
 
 bound_data = bind_rows(joined_tab) %>% as_tibble() %>%  
-mutate(id = as.factor(id))
+mutate(id = as.factor(id), interpretations = as.character(interpretations))
 
 # bound_data = bound_data 
 
@@ -193,22 +198,25 @@ mutate(id = as.factor(id))
 barThicknessMeas = bound_data %>% group_by(id, interpretations) %>% 
 summarize(meas_type = 'thickness', meas_a = diff(range(Z))) 
 
-# this function calculates the distance along the mapped linear feature.
 barFaceLengthMeas = bound_data %>% group_by(id) %>% 
 summarize(meas_type = 'face_length', meas_a = barfaceDistFun(X,Y,Z)) 
 
+# TODO: find a way to calculate the centroid of every id, and put it into loc_x loc_y and loc_z but in WGS84
+
 measurements = bind_rows(barThicknessMeas, barFaceLengthMeas)
 
-model_data = inner_join(no_geom, measurements, by = 'id')
+model_data = left_join(measurements, no_geom, by = 'id') %>% as_tibble() %>% select(-id)
 
-# wrangle shapefile data so that it matches field_data format
+# wrangle shapefile data so that it matches field_data format with whatever is required
 
-
+model_data %>% mutate(units_a = 'm')
 
 # join up with the original data
 
 comb_data = bind_rows(field_data, model_data)
 
+comb_data = comb_data %>% mutate(unique_id = 1:nrow(.))
+
 #	export as saved data file
 
-save(comb_data, file = here('data','derived_data', 'piceance_paleoslope_data.rda'))
+save(comb_data, file = here('data','derived_data', 'piceance_paleoslope_data_combined.rda'))
