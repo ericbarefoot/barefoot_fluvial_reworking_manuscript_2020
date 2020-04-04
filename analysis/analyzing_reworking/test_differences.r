c7e7687f95ffdeb609d8f8a5de94ddf0a350db5d
Eric Barefoot <eabarefoot@gmail.com>
2020-04-03 19:30:15 -0500

    feat: add way to avoid recalculating stats by loading cached data

--- analysis/analyzing_reworking/test_differences.r
+++ analysis/analyzing_reworking/test_differences.r
@@ -25,22 +25,57 @@ chamb_model = readRDS(file = here('data','derived_data', 'chamberlin_stat_model.
 model = readRDS(file = here('data','derived_data','piceance_3d_model_data.rds'))
 field = readRDS(file = here('data','derived_data','piceance_field_data.rds'))
 combined = readRDS(file = here('data', 'derived_data', 'piceance_field_model_data.rds'))
-barpres = readRDS(file = here('data', 'derived_data', 'piceance_bar_preservation.rds'))
+boots = readRDS(file = here('data', 'derived_data', 'piceance_bar_preservation.rds'))
 
 # bootstrap sampling already done in calc_3D_model_stats.r
 
-# but maybe we want fewer resamples. modified script to only take four resamples. 
+# but maybe we want fewer resamples. modified script to only take four resamples.
 
 source(here('analysis', 'analyzing_reworking', 'calc_3D_model_stats.r'))
 
-barpres = readRDS(file = here('data', 'derived_data', 'piceance_bar_preservation.rds'))
+barpres = model %>% filter(interpretations %in% c('full','partial','truncated')) %>%
+transmute(formation = case_when(
+  formation == 'molina' ~ 'molina',
+  TRUE ~ 'bounding'),
+  interp = case_when(
+  interpretations == 'full' ~ 'full',
+  TRUE ~ 'partial')
+)
 
-barpres = barpres %>% mutate(formation = as.factor(formation))
+data_tab = table(barpres$interp, barpres$formation)
+
+barpres_test = chisq.test(data_tab)
 
 # quick hypothesis test since these are normally distributed.
 
-fit = aov(perc_full ~ formation, data = barpres)
+# fit = aov(perc_full ~ formation, data = barpres)
 
-p_comp_reworking = summary(fit)[[1]][["Pr(>F)"]][1]
+# p_comp_reworking = summary(fit)[[1]][["Pr(>F)"]][1]
+
+if (barpres_test$p.value > 0.001) {
+  p_comp_reworking = paste('p =', barpres_test$p.value)
+} else {
+  p_comp_reworking = paste('p \\ll 0.001')
+}
 
 # save(p_comp_reworking, file = here('data', 'data_summaries', 'reworking_pvalue.rda'))
+
+barfractions = model %>% filter(interpretations %in% c('full','partial','truncated')) %>%
+group_by(interpretations, formation) %>% summarize(n = n()) %>%
+pivot_wider(values_from = n, names_from = interpretations) %>%
+mutate(perc = 100 * full / (full + partial + truncated))
+
+shirePerc = barfractions %>% filter(formation == 'shire') %>% pull(perc)
+molinaPerc = barfractions %>% filter(formation == 'molina') %>% pull(perc)
+atwellPerc = barfractions %>% filter(formation == 'atwell_gulch') %>% pull(perc)
+
+boot_sds = boots %>% group_by(formation) %>% summarize(sdPres = sd(perc_full) * 100)
+
+estimates = tibble(meanPres = c(shirePerc,molinaPerc,atwellPerc), formation = c('shire','molina','atwell_gulch'))
+
+pres = inner_join(boot_sds, estimates, by = 'formation')
+
+save(p_comp_reworking,
+pres,
+barpres_test,
+file = here('data', 'data_summaries', 'paleo_pres.rda'))
--- analysis/paleo_geoms/test_differences.r
+++ analysis/paleo_geoms/test_differences.r
@@ -20,6 +20,15 @@ if (interactive()) {
     require(colorout)
 }
 
+format_function = function(pval, signif = 0.001) {
+  if (pval > signif) {
+    prep = paste('p =', signif(pval, 3))
+  } else {
+    prep = paste('p \\ll', signif)
+  }
+  return(prep)
+}
+
 # load data
 
 # chamb_data = readRDS(file = here('data','derived_data', 'chamberlin_model_data.rds'))
@@ -29,7 +38,7 @@ field = readRDS(file = here('data','derived_data','piceance_field_data.rds'))
 combined = readRDS(file = here('data', 'derived_data', 'piceance_field_model_data.rds'))
 # barpres = readRDS(file = here('data', 'derived_data', 'piceance_bar_preservation.rds'))
 
-field_depths = field %>% filter(structure %in% c('bar','channel') & meas_type %in% c('thickness', 'dimensions')) %>% mutate(meas_a = case_when(meas_type == 'thickness' ~ meas_a, meas_type == 'dimensions' ~ meas_b), formation = as.factor(formation)) 
+field_depths = field %>% filter(structure %in% c('bar','channel') & meas_type %in% c('thickness', 'dimensions')) %>% mutate(meas_a = case_when(meas_type == 'thickness' ~ meas_a, meas_type == 'dimensions' ~ meas_b), formation = as.factor(formation))
 
 # quick hypothesis test since these are normally distributed.
 
@@ -39,10 +48,11 @@ formations = field_depths$formation
 fit = conover.test(depths, formations, method = 'bonferroni', table = 'false', kw = 'false')
 # fit = conover.test(depths, formations, method = 'bonferroni')
 
-p_comp_AG_M = fit$P[1]
-p_comp_AG_S = fit$P[2]
-p_comp_S_M = fit$P[3]
+p_comp_AG_M = format_function(fit$P[1])
+p_comp_AG_S = format_function(fit$P[2])
+p_comp_S_M = format_function(fit$P[3])
 
+depth_summary = field_depths %>% group_by(formation) %>% summarize(meanDepth = mean(meas_a), sdDepth = sd(meas_a))
 
 model_depths = model %>% filter(interpretations %in% c('full'))
 
@@ -50,19 +60,19 @@ all_depths = bind_rows(field_depths, model_depths, .id = 'data_source') %>% muta
 
 all_depths = all_depths %>% mutate(formID = case_when(formation == 'ohio_creek' ~ 3, formation == 'atwell_gulch' ~ 2, formation == 'molina' ~ 1, formation == 'shire' ~ 0))
 
-bedloadSamples = field %>% 
-filter(meas_type == 'sample' & !(structure %in% c('outcrop', 'formation', 'bar_drape')) & meas_a < 2000) %>% 
-mutate(D50 = meas_a * 1e-6) %>% 
+bedloadSamples = field %>%
+filter(meas_type == 'sample' & !(structure %in% c('outcrop', 'formation', 'bar_drape')) & meas_a < 2000) %>%
+mutate(D50 = meas_a * 1e-6) %>%
 mutate(formID = case_when(
-  formation == 'ohio_creek' ~ 3, 
-  formation == 'atwell_gulch' ~ 2, 
-  formation == 'molina' ~ 1, 
+  formation == 'ohio_creek' ~ 3,
+  formation == 'atwell_gulch' ~ 2,
+  formation == 'molina' ~ 1,
   formation == 'shire' ~ 0))
 
 samples_for_slope = bedloadSamples %>% select(set_id, D50, textures, composition, samp_ind, samp_code, samp_description)
 depths_for_slope = all_depths %>% filter(data_source == 'field') %>% select(-c(textures, composition, samp_ind, samp_code, samp_description))
 
-paleohydroset = inner_join(samples_for_slope, depths_for_slope, by = 'set_id') %>% 
+paleohydroset = inner_join(samples_for_slope, depths_for_slope, by = 'set_id') %>%
 mutate(S = trampush_slp(D50, meas_a))
 
 # slope tests
@@ -73,8 +83,19 @@ formations = paleohydroset$formation
 fit_slope = conover.test(slopes, formations, method = 'bonferroni', table = 'false', kw = 'false')
 # fit_slope = conover.test(slopes, formations, method = 'bonferroni')
 
-p_comp_AG_M_slope = fit_slope$P[1]
-p_comp_AG_S_slope = fit_slope$P[2]
-p_comp_S_M_slope = fit_slope$P[3]
+p_comp_AG_M_slope = format_function(fit_slope$P[1])
+p_comp_AG_S_slope = format_function(fit_slope$P[2])
+p_comp_S_M_slope = format_function(fit_slope$P[3])
 
-# save(p_comp_reworking, file = here('data', 'data_summaries', 'paleo_geom.rda'))
+slope_summary = paleohydroset %>% group_by(formation) %>% summarize(meanSlope = mean(S), sdSlope = sd(S))
+
+geoms = inner_join(slope_summary, depth_summary, by = 'formation')
+
+save(p_comp_AG_M_slope,
+p_comp_AG_S_slope,
+p_comp_S_M_slope,
+p_comp_AG_M,
+p_comp_AG_S,
+p_comp_S_M,
+geoms,
+file = here('data', 'data_summaries', 'paleo_geom.rda'))
